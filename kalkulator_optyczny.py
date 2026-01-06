339d2e0946a93bae18dbea42cf3e9c89b676ca30
Zosia Porada <zporada@student.agh.edu.pl>
2026-01-06 12:45:46 +0100

    nowy plik z projektem

---
+++ kalkulator_optyczny.py
@@ -0,0 +1,12 @@
+# KALKULATOR OPTYCZNY (Z WIZUALIZACJĄ)
+# Program powinien umożliwiać analizę prostych układów optycznych w oparciu o prawo załamania światła oraz konstrukcję obrazów w soczewkach cienkich. Program pozwala na ustawienie rodzaju soczewki, jej ogniskowej oraz położenia przedmiotu, a następnie wizualizuje bieg promieni oraz powstający obraz.
+# Funkcje programu:
+# - Wczytywanie parametrów soczewki (zbieżna/rozbieżna, ogniskowa)
+# - Wczytywanie parametrów przedmiotu (wysokość, odległość od soczewki)
+# - Obliczanie położenia obrazu oraz powiększenia liniowego
+# - Rysowanie schematu optycznego:
+# -- oś optyczna,
+# -- soczewka,
+# -- promienie główne (równoległy, przechodzący przez ognisko, przez środek soczewki)
+# - Wyświetlanie położenia i wielkości obrazu
+# - (Opcjonalnie) Obsługa układu dwóch soczewek
